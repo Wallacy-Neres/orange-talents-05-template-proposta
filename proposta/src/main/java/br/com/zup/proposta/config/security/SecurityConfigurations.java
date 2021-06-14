b4e8d0211cfcdd998cb1770086fd40bfcbb40ad9
Wallacy Silva <wallacyc11@gmail.com>
2021-06-14 18:41:25 -0300

    Login via senha cartão

---
+++ proposta/src/main/java/br/com/zup/proposta/config/security/SecurityConfigurations.java
@@ -0,0 +1,35 @@
+package br.com.zup.proposta.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests(authorize -> authorize
+	            .anyRequest().authenticated()
+		)
+		.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+	}
+	
+
+
+
+	//Configurações de recusos estaticos == JS, CSS, IMAGENS, etc.
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+        .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+	}
+	
+}
